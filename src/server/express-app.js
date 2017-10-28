b930bf4daeb3797c2443dec96b2c03d0ba65bd18
taseenb <esteban.almiron@gmail.com>
2017-10-28 17:43:44 +0100

    update

--- src/server/express-app.js
+++ src/server/express-app.js
@@ -1,18 +1,18 @@
 const config = require('./config')
 const express = require('express')
-const bodyParser = require('body-parser')
-const compression = require('compression')
+// const bodyParser = require('body-parser')
+// const compression = require('compression')
 // const debug = require('debug')('app:main')// Site router
 const siteRouter = require('./site')
 
 const app = express()
 
 app.set('view engine', 'ejs')
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
 
 if (config.env === 'production') {
-  app.use(compression())
-  app.enable('view cache')
+  // app.use(compression())
+  // app.enable('view cache')
 }
 
 // Static files
--- src/server/index.js
+++ src/server/index.js
@@ -4,33 +4,29 @@ const app = require('./express-app')
 const debug = require('debug')('app:www')
 const http = require('http')
 
-/**
- * Get port from environment and store in Express.
- */
-const port = normalizePort(config.port)
+// const port = normalizePort(config.port)
+const port = config.port
 app.set('port', port)
 
-/**
- * Create HTTP server.
- */
 const server = http.createServer(app)
 
-/**
- * Listen on provided port, on all network interfaces.
- */
-server.listen(app.get('port'))
-server.on('error', onError)
-server.on('listening', onListening)
+server.listen(app.get('port'), () => {
+  const addr = server.address()
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
+  console.log('Listening on ' + bind)
+})
 
-/**
- * Event listener for HTTP server "error" event.
- */
+server.on('error', onError)
+// server.on('listening', onListening)
+
 function onError (error) {
   if (error.syscall !== 'listen') {
     throw error
   }
 
   const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
+
+  console.log(error.code)
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
@@ -45,30 +41,24 @@ function onError (error) {
   }
 }
 
-/**
- * Normalize a port into a number, string, or false.
- */
-function normalizePort (val) {
-  const port = parseInt(val, 10)
+// function normalizePort (val) {
+//   const port = parseInt(val, 10)
 
-  if (isNaN(port)) {
-    // named pipe
-    return val
-  }
+//   if (isNaN(port)) {
+//     // named pipe
+//     return val
+//   }
 
-  if (port >= 0) {
-    // port number
-    return port
-  }
+//   if (port >= 0) {
+//     // port number
+//     return port
+//   }
 
-  return false
-}
+//   return false
+// }
 
-/**
- * Event listener for HTTP server "listening" event.
- */
-function onListening () {
-  const addr = server.address()
-  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
-  debug('Listening on ' + bind)
-}
+// function onListening () {
+//   const addr = server.address()
+//   const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
+//   debug('Listening on ' + bind)
+// }
