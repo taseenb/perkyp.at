a2c37e169200f4afb83a1b8c8554962167f57441
taseenb <esteban.almiron@gmail.com>
2017-10-28 18:10:11 +0100

    update

--- src/server/config.js
+++ src/server/config.js
@@ -5,7 +5,7 @@ console.log(process.env)
 module.exports = {
   // mongodbUri: 'mongodb://localhost:27017/test',
   env: env.NODE_ENV || 'development',
-  port: env.PORT || 8088,
+  port: env.PORT || 8080,
   host: env.HOST || '0.0.0.0',
   get serverUrl () {
     return `http://${this.host}:${this.port}`
--- src/server/index.js
+++ src/server/index.js
@@ -1,46 +1,45 @@
 // require('source-map-support').install()
 const config = require('./config')
 const app = require('./express-app')
-const debug = require('debug')('app:www')
-const http = require('http')
+// const debug = require('debug')('app:www')
+// const http = require('http')
 
 // const port = normalizePort(config.port)
-const port = config.port
-app.set('port', port)
+// const port = config.port
+// app.set('port', port)
 
+// const server = http.createServer(app)
 
-const server = http.createServer(app)
-
-server.listen(app.get('port'), () => {
-  const addr = server.address()
-  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
-  console.log('Listening on ' + bind)
+app.listen(config.port, () => {
+  // const addr = server.address()
+  // const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
+  console.log('Listening on ' + config.port)
 })
 
-server.on('error', onError)
+// server.on('error', onError)
 // server.on('listening', onListening)
 
-function onError (error) {
-  if (error.syscall !== 'listen') {
-    throw error
-  }
+// function onError (error) {
+//   if (error.syscall !== 'listen') {
+//     throw error
+//   }
 
-  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
+//   const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
 
-  console.log(error.code)
+//   console.log(error.code)
 
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges')
-      process.exit(1)
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use')
-      process.exit(1)
-    default:
-      throw error
-  }
-}
+//   // handle specific listen errors with friendly messages
+//   switch (error.code) {
+//     case 'EACCES':
+//       console.error(bind + ' requires elevated privileges')
+//       process.exit(1)
+//     case 'EADDRINUSE':
+//       console.error(bind + ' is already in use')
+//       process.exit(1)
+//     default:
+//       throw error
+//   }
+// }
 
 // function normalizePort (val) {
 //   const port = parseInt(val, 10)
