46f1212556964ea7ebcccaa3d19be42a21b90aa1
taseenb <esteban.almiron@gmail.com>
2017-11-05 03:53:33 +0000

    update

--- src/server/config.js
+++ src/server/config.js
@@ -2,6 +2,6 @@ const env = process.env
 
 module.exports = {
   env: env.NODE_ENV || 'development',
-  port: env.PORT || 7777,
+  port: 7777, //  process.env.PORT in Heroku is used by NGINX!
   host: env.HOST || '0.0.0.0'
 }
