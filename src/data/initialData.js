f5e21eb52eb49b2880de07d67d2fd754448e8fef
taseenb <esteban.almiron@gmail.com>
2018-03-05 00:44:10 +0000

    update

--- src/data/initialData.js
+++ src/data/initialData.js
@@ -4,7 +4,7 @@ const pages = require('./pages')
 module.exports = {
   title: 'Perky Pat',
   metaKeywords: 'Esteban, Almiron, London, freelance, front end, frontend, begatto kitchen, perky pat, perky pat designs ltd, free lance, developer, javascript, coder, creative, artist, programmer, contractor, three.js, webgl, canvas, senior, installation, interactive, experienced, expert, art, generative, technology, designer, site, website, web, design, site, web development, jquery, html, html5, css, css3, interactive, webdesign, portfolio',
-  metaDescription: 'Digital artist, developer, front end - Freelance',
+  metaDescription: 'Digital artist, front end developer',
   ogTitle: 'Perky Pat',
   ogDescription: 'Art, code',
   ogImage: 'assets/main.jpg',
